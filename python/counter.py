cb59d802d65b9949d0711ad2e8fc3094a30426da
cfaessler <cfaessle@hsr.ch>
2013-04-10 23:02:08 +0200

    Debug messages entfernt. DocStrings hinzugefuegt

--- python/counter.py
+++ python/counter.py
@@ -1,3 +1,9 @@
+"""
+Sample for counting button presses on rising edges.
+Button inputs are taken from stdin: on -> pressed, anything else -> not pressed
+Prints out the current count of button presses after successful increment
+"""
+
 import shelve
 import sys
 
@@ -7,16 +13,14 @@ class Button:
         pass
 
     def read(self):
+        """Reads Button state, do not forget to debounce..."""
         print "Please give input"
         reading = sys.stdin.readline()
-        if "on" in reading:
-            print "returning true"
+        if 'on' in reading:
             return True
-        print "returning false"
         return False
 
 
-counter = 0
 filename = 'db.dat'
 d = shelve.open(filename)
 if not 'count' in d:
@@ -24,17 +28,15 @@ if not 'count' in d:
 button = Button()
 
 while True:
-    print "reading button"
     button_input = button.read()
     if button_input is True:
         #Rising Edge
-        print "increment count"
         d['count'] = int(d['count']) + 1
         if d['count'] == 256:
             d['count'] = 0
 
         while button.read() is True:
-            print "awaiting falling edge"
+            print 'awaiting falling edge'
             pass
         d.sync()
-        print "count is " + str(d['count'])
+        print 'count is ' + str(d['count'])
