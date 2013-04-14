1e00461f13e8116d6e77805da0c7fb9ccb882a97
Danilo Bargen <gezuru@gmail.com>
2013-04-15 00:14:25 +0200

    Improved sevensegment.py

--- python/sevensegment.py
+++ python/sevensegment.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import
+
 import time
-import serial
+import logging
 from collections import deque
 from itertools import islice
+
+import serial
 
 
 class Segments(object):
@@ -39,7 +42,7 @@ class SevenSegmentDisplay(object):
           -----   . 8
             4
 
-    To display one "1", you need to light up segments 2,5 => in binary: 0b01001000
+    To display one "7", you need to light up segments 5,6,7 => in binary: 0b01110000
 
     """
     CHAR_MAP = {  # Don't forget to convert those values to base-2 integers!
@@ -50,13 +53,14 @@ class SevenSegmentDisplay(object):
         'f': '01000111', 'g': '01111011', 'h': '00010111', 'H': '00110111',
         'i': '00000100', 'I': '00000110', 'j': '00111000', 'k': '00001101',
         'l': '00001110', 'm': '00000000', 'n': '00010101', 'o': '00011101',
-        'O': '01111110', 'p': '01100111', 'q': '01110011', 'r': '00000101',
-        's': '01011011', 't': '00001111', 'u': '00011100', 'U': '00111110',
-        'y': '00110011', 'z': '01101101', ' ': '00000000', '.': '10000000',
-        '-': '00000001', '_': '00001000', '=': '00001001',
+        'O': '01111110', 'ö': '01011101', 'p': '01100111', 'q': '01110011',
+        'r': '00000101', 's': '01011011', 't': '00001111', 'u': '00011100',
+        'U': '00111110', 'y': '00110011', 'z': '01101101', ' ': '00000000',
+        '.': '10000000', '-': '00000001', '_': '00001000', '=': '00001001',
+        '!': '10100000',
     }
 
-    def __init__(self, device='/dev/ttyUSB0', digits=8):
+    def __init__(self, device='/dev/ttyUSB0', digits=8, timeout=1):
         """Initialize a new 7 segment display on a serial port.
 
         Args:
@@ -64,9 +68,11 @@ class SevenSegmentDisplay(object):
                 Device path to serial device.
             digits:
                 Number of digits in the display.
+            timeout:
+                The writeTimeout. See pyserial docs for more information. (Default: 1).
 
         """
-        self.ser = serial.Serial(device, 9600, timeout=1)
+        self.ser = serial.Serial(device, 9600, timeout=timeout, writeTimeout=timeout)
         self.digits = digits
         time.sleep(2)  # Wait for serial device to become ready
 
@@ -74,7 +80,7 @@ class SevenSegmentDisplay(object):
         """Write frames to the display. If there are less frames than digits,
         "blank" digits will be appended on the right side."""
 
-        print(frames)
+        logging.debug('Writing {}'.format(frames))
 
         # Pad display with "blank digits"
         if len(frames) < self.digits:
@@ -95,17 +101,32 @@ class SevenSegmentDisplay(object):
         return frame
 
     def write_string(self, string):
+        """Write a string with max length ``self.digits`` to the display."""
         assert len(string) <= self.digits, 'Max string length: {}'.format(self.digits)
         frame = self._convert_string(string)
         self.write(frame)
 
-    def rotate_string(self, string, delay=0.2):
+    def rotate_string(self, string, delay=0.2, repeat=-1):
+        """Write a scrolling/rotating string to the display.
+
+        Args:
+            string:
+                The string to write to the display.
+            delay:
+                Delay time between a shift (Default: 0.2).
+            repeat:
+                How many times to repeat the string. Use ``-1`` for infinite
+                scrolling. (Default: -1).
+        """
         frame = deque(self._convert_string(string))
-        while 1:
+        shifts = repeat * len(string) + 1 if repeat > 0 else 1
+        while shifts > 0:
             length = min(8, len(frame))
             self.write(list(islice(frame, 0, length)))
             frame.rotate(-1)
             time.sleep(delay)
+            if repeat > 0:
+                shifts -= 1
 
     @classmethod
     def get_char(cls, char):
