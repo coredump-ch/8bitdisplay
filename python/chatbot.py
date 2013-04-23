b048d720585d358efd6ab92718ec3757cdc05ee4
Danilo Bargen <gezuru@gmail.com>
2013-04-23 21:16:02 +0200

    Jonas/Chrigi -> Mario/Pauline

--- python/chatbot.py
+++ python/chatbot.py
@@ -2,14 +2,14 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
 from cleverbot import cleverbot
 
-jonas = cleverbot.Cleverbot()
-chrigi = cleverbot.Cleverbot()
+mario = cleverbot.Cleverbot()
+pauline = cleverbot.Cleverbot()
 initial_question = 'Hello. Do you like the 8 bit bar?'
 
-resp1 = jonas.ask(initial_question)
-print('Jonas: {}'.format(initial_question))
+resp1 = mario.ask(initial_question)
+print('Mario: {}'.format(initial_question))
 while 1:
-    print('Chrigi: {}'.format(resp1))
-    resp2 = chrigi.ask(resp1)
-    print('Jonas: {}'.format(resp2))
-    resp1 = jonas.ask(resp2)
+    print('Pauline: {}'.format(resp1))
+    resp2 = pauline.ask(resp1)
+    print('Mario: {}'.format(resp2))
+    resp1 = mario.ask(resp2)
