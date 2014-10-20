d3e8c234cd4a733b5c5d50b4801d53fdb765c452
Danilo Bargen <mail@dbrgn.ch>
2014-10-20 21:44:20 +0200

    Print coredump

---
+++ arduino/src/charmap.h
@@ -0,0 +1,68 @@
+/*
+
+Segment numbering:
+       7
+    --------
+   |        |
+ 2 |        | 6
+   |    1   |
+    --------
+   |        |
+ 3 |        | 5
+   |        |
+    --------   . 8
+       4
+       
+       
+To display a one "1", you need to light up segments 5,6
+=> in binary: 0b00110000
+
+*/
+
+#define CHAR_0 0b01111110
+#define CHAR_1 0b00110000
+#define CHAR_2 0b01101101
+#define CHAR_3 0b01111001
+#define CHAR_4 0b00110011
+#define CHAR_5 0b01011011
+#define CHAR_6 0b01011111
+#define CHAR_7 0b01110000
+#define CHAR_8 0b01111111
+#define CHAR_9 0b01111011
+#define CHAR_a 0b01110111
+#define CHAR_b 0b00011111
+#define CHAR_c 0b00001101
+#define CHAR_C 0b01001110
+#define CHAR_d 0b00111101
+#define CHAR_e 0b01001111
+#define CHAR_f 0b01000111
+#define CHAR_g 0b01111011
+#define CHAR_h 0b00010111
+#define CHAR_H 0b00110111
+#define CHAR_i 0b00000100
+#define CHAR_I 0b00000110
+#define CHAR_j 0b00111000
+#define CHAR_k 0b00001101
+#define CHAR_l 0b00001110
+#define CHAR_m 0b01110110
+#define CHAR_n 0b00010101
+#define CHAR_o 0b00011101
+#define CHAR_O 0b01111110
+#define CHAR_ouml 0b01011101
+#define CHAR_p 0b01100111
+#define CHAR_q 0b01110011
+#define CHAR_r 0b00000101
+#define CHAR_s 0b01011011
+#define CHAR_t 0b00001111
+#define CHAR_u 0b00011100
+#define CHAR_U 0b00111110
+#define CHAR_uuml 0b01011100
+#define CHAR_y 0b00110011
+#define CHAR_z 0b01101101
+#define CHAR_SPACE 0b00000000
+#define CHAR_DOT 0b10000000
+#define CHAR_MINUS 0b00000001
+#define CHAR_UNDERSCORE 0b00001000
+#define CHAR_EQUAL 0b00001001
+#define CHAR_EXCLAMATION 0b10100000
+#define CHAR_COMMA 0b10000000
--- arduino/src/mainloop.ino
+++ arduino/src/mainloop.ino
@@ -1,7 +1,11 @@
 #include "LedControl.h"
+#include <src/charmap.h>
 #define DIN 11
 #define CS 12
 #define CLOCK 13
+#define ADDR 0
+
+#define SERIAL_INTERFACE false
 
 /*
 Segment numbering:
@@ -21,48 +25,72 @@ Segment numbering:
 To display a one "1", you need to light up segments 5,6 => in binary: 0b00110000
 */
 
+// Initialization
+
 LedControl lc = LedControl(DIN,CLOCK,CS,1); 
 char display_buffer[8];
 
-void setup(){
-  lc.shutdown(0,false);
-  lc.setIntensity(0,15);
-  Serial.begin(9600);
+
+// Function declarations
+
+static void print8bit();
+static void printCoredump();
+static void printBuffer();
+
+
+// Setup & Loop
+
+void setup() {
+    lc.shutdown(0,false);
+    lc.setIntensity(0,15);
+#ifdef SERIAL_INTERFACE
+    Serial.begin(9600);
+#endif
 }
 
-void loop(){
-  print8bit();
-  delay(300);
-  Serial.println("8bit Display is ready!");
-  while(1){
-    if(Serial.available()>=8){
-      Serial.readBytes(display_buffer,8);
-      Serial.print("New string received: ");
-      printBuffer();
+void loop() {
+    printCoredump();
+
+#ifdef SERIAL_INTERFACE
+    Serial.println("8bit Display is ready!");
+    while (1){
+        if (Serial.available() >= 8){
+            Serial.readBytes(display_buffer, 8);
+            Serial.print("New string received: ");
+            printBuffer();
+        }
     }
-  }
-}
-
-void print8bit(){
-  lc.setDigit(0,7,8,false);
-  lc.setRow(0,6,0b000011111);
-  lc.setRow(0,5,0b100);
-  lc.setRow(0,4,0b111);
-  lc.setRow(0,3,0x00);
-  lc.setRow(0,2,0b000011111);
-  lc.setDigit(0,1,0xa,false);
-  lc.setRow(0,0,0b101);
-}
-
-void printBuffer(){
-  for(int i =7; i>=0; --i){
-    lc.setRow(0,i,display_buffer[7-i]);
-    Serial.write(display_buffer[7-i]);
-  }
+#endif
 }
 
 
+// Output functions
 
+void print8bit() {
+    lc.setDigit(ADDR, 7, 8, false);
+    lc.setRow(ADDR, 6, 0b000011111);
+    lc.setRow(ADDR, 5, 0b100);
+    lc.setRow(ADDR, 4, 0b111);
+    lc.setRow(ADDR, 3, 0x00);
+    lc.setRow(ADDR, 2, 0b000011111);
+    lc.setDigit(ADDR, 1, 0xa, false);
+    lc.setRow(ADDR, 0, 0b101);
+}
 
+void printBuffer() {
+    for(int i =7; i>=0; --i){
+        lc.setRow(0, i, display_buffer[7-i]);
+        Serial.write(display_buffer[7-i]);
+    }
+}
 
-
+void printCoredump() {
+    lc.setRow(ADDR, 7, CHAR_C);
+    lc.setRow(ADDR, 6, CHAR_o);
+    lc.setRow(ADDR, 5, CHAR_r);
+    lc.setRow(ADDR, 4, CHAR_e);
+    lc.setRow(ADDR, 3, CHAR_d);
+    lc.setRow(ADDR, 2, CHAR_u);
+    lc.setRow(ADDR, 1, CHAR_m);
+    lc.setRow(ADDR, 0, CHAR_p);
+}
